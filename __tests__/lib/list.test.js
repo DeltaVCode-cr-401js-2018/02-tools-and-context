4cced783acc2df176567487797ba883e58d603f0
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-14 15:48:43 -0500

    added test for length

--- __tests__/lib/list.test.js
+++ __tests__/lib/list.test.js
@@ -1,3 +1,12 @@
 'use strict';
 
 const List = require('../../lib/list');
+
+describe('List', () => {
+  describe('constructor',()=> {
+    it('has length of zero',()=>{
+      var list = new List();
+
+      expect(list.length).toBe(0);
+    });
+  });
